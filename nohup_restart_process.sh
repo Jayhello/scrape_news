260eeb2e23447ac3cd72c98481d691e467e964c8
Jayhello <haluoluo@qq.com>
2017-08-28 17:27:26 +0800

    1.change some contents of the nohup_restart_process.sh

--- nohup_restart_process.sh
+++ nohup_restart_process.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #restart scrape news process if the process exited accidentally
 
-log_file="restart_scrape.log"
+log_file="restart_sh.log"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
 stop_process_if_running(){
 	# $1->process_name to grep
 	echo $(ps -ef | grep $1)
-	is_running=$(ps -ef | grep $1 | wc -l)
-	if [ $is_running -gt 0 ]
+	be_running=$(ps -ef | grep $1 | wc -l)
+	if [ $be_running -gt 0 ]
 	then
 		echo "$TIMESTAMP $1 is running, T'm going to kill it"
-		kill -9 $(ps -ef | grep "$1" | awk '{print $2}')
+		ps -ef | grep "$1" | awk '{print $2}' | xargs kill -9
 		if [ $? -eq 0 ];
 		then
 			echo "kill $1 successfully!!!"
@@ -28,13 +28,13 @@ restart_process_if_die(){
 	be_running=$(ps -ef | grep $1 | wc -l)
 	if [ $be_running -eq 0 ];
 	then
-		echo "$TIMESTAMP $3 got down, now I will restart it" | tee -a restart_sh.log
+		echo "$TIMESTAMP $3 got down, now I will restart it" | tee -a $log_file
 		cd $2
 		echo "Now I am in $PWD"
 		nohup python $3 & 2>&1
 		if [ $? -eq 0 ];
 		then
-			echo "$TIMESTAMP $3 restart successfully" | tee -a restart_sh.log
+			echo "$TIMESTAMP $3 restart successfully" | tee -a $log_file
 		fi
 		cd -
 	else
@@ -42,17 +42,16 @@ restart_process_if_die(){
 	fi
 }
 
-#when execute this shell script, if the process is running,kill it firstly
 test_process="[p]ython.*nohup_restart_test_py"
-stop_process_if_running $test_process
 file_dir=/home/xiongyu/search_start_sh/
 py_file=nohup_restart_test_py.py
+#when execute this shell script, if the process is running,kill it firstly
+stop_process_if_running $test_process
 
-# check if the process is died, if got died then restart it
+# poll if the process is died, if got died then restart it.
 while :
 do
 	restart_process_if_die $test_process $file_dir $py_file
 	echo "$TIMESTAMP now I will sleep 10S"
 	sleep 10
 done
-
